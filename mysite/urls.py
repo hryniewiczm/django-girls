397ed2c76a2807987b0794d1ad1bf2ab93e1912b
hryniewiczm <hryniewicz.m@o2.pl>
2018-08-26 15:47:02 +0200

    koniec drugiego dnia

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
